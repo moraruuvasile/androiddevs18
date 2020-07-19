d24dac4997c7e3d5e435d4c0a7701859ff3bdbf4
Vasile <vaseoa@yahoo.com>
2020-07-19 17:58:36 +0300

    navigation

--- app/src/main/java/com/example/androiddevs18/ui/MainActivity.kt
+++ app/src/main/java/com/example/androiddevs18/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.androiddevs18
+package com.example.androiddevs18.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androiddevs18.R
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
