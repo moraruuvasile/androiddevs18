31d0dc1c4ac13319147f111482f7e69e5e487b8d
Vasile <vaseoa@yahoo.com>
2020-07-19 21:58:56 +0300

    retrofit

--- app/src/main/java/com/example/androiddevs18/ui/MainActivity.kt
+++ app/src/main/java/com/example/androiddevs18/ui/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.androiddevs18.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.androiddevs18.R
+import kotlinx.android.synthetic.main.activity_news.*
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_news)
+		bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+
 	}
 }
