f4249b249d7feda887893d5d63eb14bd6ab85adc
Vasile <vaseoa@yahoo.com>
2020-07-19 22:19:36 +0300

    db

---
+++ app/src/main/java/com/example/androiddevs18/db/ArticleDao.kt
@@ -0,0 +1,18 @@
+package com.example.androiddevs18.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.androiddevs18.model.Article
+
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
--- app/src/main/java/com/example/androiddevs18/model/NewsResponse.kt
+++ app/src/main/java/com/example/androiddevs18/model/NewsResponse.kt
@@ -1,12 +1,21 @@
 package com.example.androiddevs18.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 data class NewsResponse(
     val articles: List<Article>,
     val status: String,
     val totalResults: Int
 )
 
+
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
     val author: String,
     val content: String,
     val description: String,
